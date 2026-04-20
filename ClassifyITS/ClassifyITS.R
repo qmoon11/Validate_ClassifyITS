290d02f4f7d0adaa7d7a9720214784a1a39ac8ea
Quinn Moon <qmoon@umich.edu>
2026-04-20 15:17:58 +0200

    Update ClassifyITS.R

--- ClassifyITS/ClassifyITS.R
+++ ClassifyITS/ClassifyITS.R
@@ -9,3 +9,15 @@ ITS_taxonomy <- ClassifyITS::ITS_assignment(
   outdir     = "ClassifyITS_outputs"
 )
 
+# Read in BLAST results and assignments
+blast <- read.table("megablast_ITS2_ClassifyITS.tsv", sep = "\t", header = TRUE) #user provided BLAST results
+assignments <- read.csv("ClassifyITS_outputs/initial_assignments.csv", stringsAsFactors = FALSE) #preliminary taxonomy assignments from ClassifyITS
+View(assignments)
+
+# Subset assignments to fungal OTUs unclassified at class level
+unclassified_class_otus <- assignments$qseqid[
+  (assignments$kingdom == "Fungi" | is.na(assignments$kingdom) | assignments$kingdom == "NA") &
+    (is.na(assignments$class) | assignments$class == "" | assignments$class == "Unclassified")
+]
+ #list of OTUs to inspect manually
+unclassified_class_otus
